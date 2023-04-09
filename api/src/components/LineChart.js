2cfe59ae9b72249acd42c2f3e8f3e284bf1f3df7
Bladeyboy54 <221146@virtualwindow.co.za>
2023-04-09 18:45:38 +0200

    working on timeline
    It wont work

--- api/src/components/LineChart.js
+++ api/src/components/LineChart.js
@@ -1,61 +1,61 @@
 import React from 'react'
 import {Line} from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
 
 function LineChart({chartData}) {
-  const [labels, setLabels] = useState([])
-  const [lineData, setLineData] = useState([])
-  const apiKey = "?key=18a129ee851a4354b439424c7f90a3f5"
+  // const [labels, setLabels] = useState([])
+  // const [lineData, setLineData] = useState([])
+  // const apiKey = "?key=18a129ee851a4354b439424c7f90a3f5"
 
-  useEffect (() => {
-    console.log("Test");
-    axios.get('https://api.rawg.io/api/developers'+apiKey)
-    .then(res => { 
-      let results = res.data.results[0].games
-      // let names = res.data.results[0].games.map(name)
-      let names = []
-      let releaseDate = []
+  // useEffect (() => {
+  //   console.log("Test");
+  //   axios.get('https://api.rawg.io/api/developers'+apiKey)
+  //   .then(res => { 
+  //     let results = res.data.results[0].games
+  //     // let names = res.data.results[0].games.map(name)
+  //     let names = []
+  //     let releaseDate = []
 
-      for (let i = 0; i < res.data.results[0].games.length; i++) {
-        names.push(res.data.results[0].games[i].name)
-        axios.get('https://api.rawg.io/api/games/'+res.data.results[0].games[i].id+apiKey)
-        .then(res1 => { 
-          console.log(res1.data.released);
-          releaseDate.push(res1.data.released)
-        })
-      }
-      console.log(names)
-      setLabels(names)
-      setLineData(releaseDate)
-      console.log(results)
-      // console.log(res.data.info)
+  //     for (let i = 0; i < res.data.results[0].games.length; i++) {
+  //       names.push(res.data.results[0].games[i].name)
+  //       axios.get('https://api.rawg.io/api/games/'+res.data.results[0].games[i].id+apiKey)
+  //       .then(res1 => { 
+  //         console.log(res1.data.released);
+  //         releaseDate.push(res1.data.released)
+  //       })
+  //     }
+  //     console.log(names)
+  //     setLabels(names)
+  //     setLineData(releaseDate)
+  //     console.log(results)
+  //     // console.log(res.data.info)
 
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  },[])
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // },[])
 
-  const developers = {
-    labels: labels,
-    options: {
-      scales: {
-        xAxes: [{
-          type: 'date'
-        }]
-      }
-    },
-    datasets: [{
-      label: 'Users gained',
-      data: lineData,
-      tension: 0.6
-    }]
-  }
+  // const developers = {
+  //   labels: labels,
+  //   options: {
+  //     scales: {
+  //       xAxes: [{
+  //         type: 'date'
+  //       }]
+  //     }
+  //   },
+  //   datasets: [{
+  //     label: 'Users gained',
+  //     data: lineData,
+  //     tension: 0.6
+  //   }]
+  // }
 
   return (
-    <Line data={developers} />
+    <Line data={chartData} />
   )
 }
 
--- api/src/pages/timeline.js
+++ api/src/pages/timeline.js
@@ -1,11 +1,61 @@
 import React from "react";
 import LineChart from "../components/LineChart";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Timeline(){
+
+    const [Labels, setLabels] = useState([])
+    const [lineData, setLineData] = useState([])
+    const apiKey = "?key=18a129ee851a4354b439424c7f90a3f5"
+
+    useEffect(() => {
+        axios.get('https://api.rawg.io/api/developers'+apiKey)
+        .then(res => {
+            console.log(res.data)
+            let results = res.data.results[0].games
+            let names = []
+            let releaseDate = []
+
+            for (let i = 0; i < res.data.results[0].games.length; i++) {
+                names.push(res.data.results[0].games[i].name)
+                axios.get('https://api.rawg.io/api/games/'+res.data.results[0].games[i].id+apiKey)
+                .then(res1 => {
+                    console.log(res1.data.released);
+                    releaseDate.push(res1.data.released)
+                })
+            }
+            console.log(names)
+            setLabels(names)
+            console.log(results)
+            setLineData(releaseDate)
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },[])
+
+    const developers = {
+        Labels: Labels,
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'data'
+                }]
+            }
+        },
+        datasets: [{
+            label: 'Labels',
+            data: lineData,
+            tension: 0.6
+        }]
+    }
+
     return(
         <> 
         <p>Timeline</p>
-        {/* <LineChart chartData={developers} /> */}
+        <LineChart chartData={developers} />
         </>
        
     )
