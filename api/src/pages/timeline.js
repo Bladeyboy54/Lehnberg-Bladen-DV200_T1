53d4486dba835f0fe7d293b3e954fa3c7b05a87d
Bladeyboy54 <221146@virtualwindow.co.za>
2023-04-11 19:31:18 +0200

    worked on timeline
    almost fixed

--- api/src/pages/timeline.js
+++ api/src/pages/timeline.js
@@ -2,10 +2,11 @@ import React from "react";
 import LineChart from "../components/LineChart";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 function Timeline(){
 
-    const [Labels, setLabels] = useState([])
+    const [labels, setLabels] = useState([])
     const [lineData, setLineData] = useState([])
     const apiKey = "?key=18a129ee851a4354b439424c7f90a3f5"
 
@@ -16,20 +17,28 @@ function Timeline(){
             let results = res.data.results[0].games
             let names = []
             let releaseDate = []
-            let releaseTimestamp = []
+            let releaseTimestamps = []
             
             for (let i = 0; i < res.data.results[0].games.length; i++) {
                 names.push(res.data.results[0].games[i].name)
                 axios.get('https://api.rawg.io/api/games/'+res.data.results[0].games[i].id+apiKey)
                 .then(res1 => {
-                    console.log(res1.data.released);
-                    releaseDate.push(res1.data.released)
+                    const releaseDate = res1.data.released
+                    console.log(releaseDate)
+                    releaseTimestamps.push(moment.utc(releaseDate).format('YYYY-MM-DD'));
+                    if (releaseTimestamps.length === res.data.results[0].games.length) {
+                        setLabels(names)
+                        console.log(releaseTimestamps)
+                        setLineData(releaseTimestamps)
+                    }
+                    // console.log(res1.data.released);
+                    // releaseDate.push(res1.data.released)
                 })
             }
-            console.log(names)
-            setLabels(names)
-            console.log(results)
-            setLineData(releaseDate)
+            // console.log(names)
+            // setLabels(names)
+            // console.log(results)
+            // setLineData(releaseDate)
 
         })
         .catch(err => {
@@ -38,17 +47,45 @@ function Timeline(){
     },[])
 
     const developers = {
-        Labels: Labels,
+        Labels: labels,
         options: {
             scales: {
                 xAxes: [{
-                    type: 'data'
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Game Name'
+                    }
+                }],
+                yAxes: [{
+                    type: 'time',
+                    time: {
+                        unit: 'year',
+                        displayFormats: {
+                            year: 'YYYY',
+                        },
+
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Release Date'
+                    },
+                    // ticks: {
+                    //     callback: function(value) {
+                    //         return moment(value).format('MMM YYYY');
+                    //     }
+                    // }
                 }]
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top'
+                }
             }
         },
         datasets: [{
             label: 'Labels',
-            data: lineData,
+            data: lineData.map((timestamp, i) => ({x: labels[i], y: moment(timestamp).valueOf() })),
             tension: 0.6
         }]
     }
